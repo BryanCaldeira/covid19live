9a2bf9ef8be1672e2ff71e4815e933fac69830d9
BryanCaldeira <63894533+BryanCaldeira@users.noreply.github.com>
2020-05-12 10:35:39 +0530

    Update urls.py

--- covid19live/urls.py
+++ covid19live/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from Main_Page import views
 
 urlpatterns = [
+    path('ap/', views.AndhraPradesh),
     path('admin/', admin.site.urls),
     path('', views.Main , name="main"),
     path('karnataka/', views.Karnataka),
     path('tamilnadu/', views.Tamilnadu),
     path('kerala/', views.Kerala),
-    path('andhrapradesh/', views.AndhraPradesh),
     path('maharashtra/', views.Maharashtra),
     path('telangana/', views.Telangana),
 
